a401661d5e458e2deca4e4b928f2dfddfa10fa8a
TAFARAK <mohammadtafarak@gmail.com>
2022-08-13 02:38:58 +0530

    init3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    //BEM
-    <div className="App">
-      <h1> </h1>
-      <Header />
-      <Home />
-
-
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+            <Route path="/checkout">
+              <Header />
+              <Checkout />
+              <p>I Am Checkout</p>
+            </Route>
+            <Route path="/">
+              <Header />
+              <Home />
+            </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
